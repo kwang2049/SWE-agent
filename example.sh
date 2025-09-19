77283e7c60ea878576ebe5463aa91f84ee560c8b
Kexin Wang <kwang@Mac.fritz.box>
2025-09-19 23:59:45 +0200

    runnable with gemini-2.0-flash-001 (by no caching)

---
+++ example.sh
@@ -0,0 +1,5 @@
+sweagent run \
+    --env.repo.github_url=https://github.com/SWE-agent/test-repo \
+    --problem_statement.github_url=https://github.com/SWE-agent/test-repo/issues/1 \
+    --agent.model.top_p null \
+    --agent.model.temperature 1
